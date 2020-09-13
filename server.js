5371a4306988b900b58ebcaa3155be6b06ad8922
manoj kumar <happinessofsorrow@gmail.com>
2020-09-14 02:30:46 +0530

    serve favicon removed

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import config from "./config";
 import bodyParser from "body-parser";
 import cors from "cors";
 import path from "path";
-import favicon from "serve-favicon";
 
 //Impoting routes
 import AuthRoute from "./Routes/auth";
@@ -31,7 +30,6 @@ app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 //Routes / Apis
 app.use("/api/auth", AuthRoute);
 app.use("/api/story", StoryRoute);
